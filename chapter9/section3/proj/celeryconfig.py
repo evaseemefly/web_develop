5cde3ff608ec9b0b8c2e2b665155a81c65642f85
liusihan <evaseemefly@126.com>
2019-02-18 12:26:42 +0800

    1-加入了部分配置

--- chapter9/section3/proj/celeryconfig.py
+++ chapter9/section3/proj/celeryconfig.py
@@ -1,7 +1,11 @@
 # coding=utf-8
+# 使用rabbitmq作为消息代理
 BROKER_URL = 'amqp://dongwm:123456@localhost:5672/web_develop'
+# 把任务结果存在redis中
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+# 任务序列化和反序列化使用msgpack方案
 CELERY_TASK_SERIALIZER = 'msgpack'
 CELERY_RESULT_SERIALIZER = 'json'
+# 任务过期时间
 CELERY_TASK_RESULT_EXPIRES = 60 * 60 * 24
 CELERY_ACCEPT_CONTENT = ['json', 'msgpack']
